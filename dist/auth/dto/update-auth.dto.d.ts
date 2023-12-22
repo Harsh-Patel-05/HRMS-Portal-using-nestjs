016bbcba46789fc431d804c21cc3207abdec5893
Harsh Patel <harsh53006@gmail.com>
2023-12-22 15:46:20 +0530

    update

--- dist/auth/dto/update-auth.dto.d.ts
+++ dist/auth/dto/update-auth.dto.d.ts
@@ -2,4 +2,4 @@ import { CreateAuthDto } from './create-auth.dto';
 declare const UpdateAuthDto_base: import("@nestjs/common").Type<Partial<CreateAuthDto>>;
 export declare class UpdateAuthDto extends UpdateAuthDto_base {
 }
-export {};
+export { };
