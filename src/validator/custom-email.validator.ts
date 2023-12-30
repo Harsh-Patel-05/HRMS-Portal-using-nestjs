131d632992cd0824a0874665222ad0015c106a29
Harsh Patel <harsh53006@gmail.com>
2023-12-30 11:56:21 +0530

    update

--- src/validator/custom-email.validator.ts
+++ src/validator/custom-email.validator.ts
@@ -10,6 +10,7 @@ export class CustomEmailValidator implements ValidatorConstraintInterface {
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z]+\.[a-zA-Z]/;
     return emailRegex.test(email);
   }
+  
 
   defaultMessage(args: ValidationArguments) {
     return 'Invalid email format (e.g., test12@gmail.com)';
