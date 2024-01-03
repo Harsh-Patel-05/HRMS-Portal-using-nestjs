9549ceee070842927999fe018c3b0bb2cb100be7
Harsh Patel <harsh53006@gmail.com>
2024-01-03 10:19:49 +0530

    update

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -3,10 +3,10 @@ import { IsNotEmpty, Validate } from "class-validator";
 import { CustomEmailValidator } from "src/validator/custom-email.validator";
 
 export class LoginDto {
-    @IsNotEmpty()
-    @Validate(CustomEmailValidator, {
-      message: 'Email format is not valid (e.g., test@gmail.com)',
-    })
-    @ApiProperty({ example: 'test@gmail.com' })
-    email: string;
-  }
+  @IsNotEmpty()
+  @Validate(CustomEmailValidator, {
+    message: 'Email format is not valid (e.g., test@gmail.com)',
+  })
+  @ApiProperty({ example: 'test@gmail.com' })
+  email: string;
+}
