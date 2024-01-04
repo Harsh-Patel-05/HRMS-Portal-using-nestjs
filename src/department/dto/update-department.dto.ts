5665b39d7b0e7a8964a4844580d900753ed12b22
Harsh Patel <harsh53006@gmail.com>
2024-01-04 18:13:13 +0530

    update

--- src/department/dto/update-department.dto.ts
+++ src/department/dto/update-department.dto.ts
@@ -2,3 +2,7 @@ import { PartialType } from '@nestjs/swagger';
 import { CreateDepartmentDto } from './create-department.dto';
 
 export class UpdateDepartmentDto extends PartialType(CreateDepartmentDto) { }
+
+
+
+
